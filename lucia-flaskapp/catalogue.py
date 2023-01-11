491d2efc15703a10cb18bd685a6e1f31c4da5bf0
Lucytreasure <117147880+Lucytreasure@users.noreply.github.com>
2023-01-11 15:51:23 +0000

    Add files via upload

---
+++ lucia-flaskapp/catalogue.py
@@ -0,0 +1,86 @@
+from datetime import *
+import time
+import sys
+
+import json
+import requests
+# First we set our credentials
+
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+app = Flask(__name__)
+
+
+@app.route('/Video/<video>')
+def video_page(video):
+    print (video)
+    url = 'http://35.233.125.61/myflix/videos?filter={"video.uuid":"'+video+'"}'
+    headers = {}
+    payload = json.dumps({ })
+    print (request.endpoint)
+    response = requests.get(url)
+    print (url)
+    if response.status_code != 200:
+      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
+      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+    jResp = response.json()
+    print (type(jResp))
+    print (jResp)
+    for index in jResp:
+        for key in index:
+           if (key !="_id"):
+              print (index[key])
+              for key2 in index[key]:
+                  print (key2,index[key][key2])
+                  if (key2=="Name"):
+                      video=index[key][key2]
+                  if (key2=="file"):
+                      videofile=index[key][key2]
+                  if (key2=="pic"):
+                      pic=index[key][key2]
+    return render_template('video.html', name=video,file=videofile,pic=pic)
+
+@app.route('/')
+def cat_page():
+    url = "http://35.233.125.61/myflix/videos"
+    headers = {}
+    payload = json.dumps({ })
+
+    response = requests.get(url)
+    #print (response)
+    # exit if status code is not ok
+    print (response)
+    print (response.status_code)
+    if response.status_code != 200:
+      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
+      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+    jResp = response.json()
+    print (type(jResp))
+    html="<h2> Your Videos</h2>"
+    for index in jResp:
+       #print (json.dumps(index))
+       print ("----------------")
+       for key in index:
+
+           if (key !="_id"):
+              print (index[key])
+              for key2 in index[key]:
+                  print (key2,index[key][key2])
+                  if (key2=="Name"):
+                      name=index[key][key2]
+                  if (key2=="thumb"):
+                      thumb=index[key][key2]
+                  if (key2=="uuid"):
+                      uuid=index[key][key2]  
+              html=html+'<h3>'+name+'</h3>'
+              ServerIP=request.host.split(':')[0]
+              html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
+              html=html+'<img src="https://storage.googleapis.com/flix-bucket/flixpics/'+thumb+'">'
+              html=html+"</a>"        
+              print("=======================")
+
+    return html
+
+
+if __name__ == '__main__':
+    app.run()
